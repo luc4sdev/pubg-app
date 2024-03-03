daf4c9c1ac23a8366286b3c1911f35b836ee43dd
lucasLivefarm <lucasgamesdark@gmail.com>
2024-03-03 13:06:52 -0300

    fix: error suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import { SearchForm } from "@/components/SearchForm";
 import Image from "next/image";
 
 import logo from '@/../../public/pubg-logo.svg'
+import { Suspense } from "react";
 export default async function Home() {
-  
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-5">
       <Image src={logo} width={300} height={300} alt="PUBG logo" />
       <h1 className="text-2xl font-bold">Insira o seu username do PUBG</h1>
-      <SearchForm />
+      <Suspense fallback={null}>
+        <SearchForm />
+      </Suspense>
     </div>
   );
 }
